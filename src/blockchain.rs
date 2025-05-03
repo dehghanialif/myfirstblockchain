df42bd5f40dc8cfe5b56bfa3a9c9687da467cf2a
Ali Dehghani <dehghanialif@gmail.com>
2025-05-03 08:06:26 +0200

    Implement initial blockchain data structures and scaffold core methods

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,20 +1,36 @@
-pub struct Transaction {}
+pub struct Transaction {
+    sender: String,
+    recipient: String,
+    amount: u64,
+}
 
-pub struct Block {}
+pub struct Block {
+    index: u64,
+    timestamp: f64,
+    transactions: Vec<Transaction>,
+    proof: u64,
+    previous_hash: String,
+}
 
-pub struct Blockchain {}
+pub struct Blockchain {
+    chain: Vec<Block>,
+    current_transactions: Vec<Transaction>,
+}
 
 impl Blockchain {
     pub fn init() -> Self {
-        Blockchain {}
+        Blockchain {
+            chain: vec![],
+            current_transactions: vec![],
+        }
     }
 
     pub fn new_block(self: &Self) {}
 
     pub fn new_transaction(self: &Self) {}
 
-    pub fn last_block(self: &Self) -> Block {
-        Block {}
+    pub fn last_block(self: &Self) -> Option<&Block> {
+        self.chain.last()
     }
 }
 
