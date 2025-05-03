e474d70e7feb667e8a415f454f5f946d65850d6d
Ali Dehghani <dehghanialif@gmail.com>
2025-05-03 07:58:40 +0200

    Add initial blockchain module structure

---
+++ src/blockchain.rs
@@ -0,0 +1,21 @@
+pub struct Transaction {}
+
+pub struct Block {}
+
+pub struct Blockchain {}
+
+impl Blockchain {
+    pub fn init() -> Self {
+        Blockchain {}
+    }
+
+    pub fn new_block(self: &Self) {}
+
+    pub fn new_transaction(self: &Self) {}
+
+    pub fn last_block(self: &Self) -> Block {
+        Block {}
+    }
+}
+
+pub fn hash() {}
